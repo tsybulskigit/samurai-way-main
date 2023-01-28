539888093af7ff4e7a1034845426480fbd7f6e9a
tsybulskigit <hzkto@mail.ru>
2023-01-29 02:46:40 +0300

    lesson 1-9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+
+const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies
